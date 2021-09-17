0b7450ad6379c45662a58d1f296e3bd6d7b52423
carles <carles.miralles@gartner.com>
2021-09-17 18:50:08 +0200

    Add integration tests for sets

--- spec/integration/outputs/redis_spec.rb
+++ spec/integration/outputs/redis_spec.rb
@@ -7,24 +7,28 @@ require "flores/random"
 describe LogStash::Outputs::Redis do
 
   context "integration tests", :integration => true do
-    shared_examples_for "writing to redis list" do |extra_config|
+    RSpec.shared_examples "writing to redis" do |extra_config|
       let(:key) { 10.times.collect { rand(10).to_s }.join("") }
       let(:event_count) { Flores::Random.integer(0..10000) }
       let(:message) { Flores::Random.text(0..100) }
       let(:default_config) {
         {
           "key" => key,
-          "data_type" => "list",
+          "data_type" => data_type,
           "host" => "localhost"
         }
       }
       let(:redis_config) {
         default_config.merge(extra_config || {})
       }
+
       let(:redis_output) { described_class.new(redis_config) }
 
       before do
         redis_output.register
+        # TODO:
+        puts "#{data_type} => #{event_count}"
+
         event_count.times do |i|
           event = LogStash::Event.new("sequence" => i, "message" => message)
           redis_output.receive(event)
@@ -36,39 +40,71 @@ describe LogStash::Outputs::Redis do
         redis = Redis.new(:host => "127.0.0.1")
 
         # The list should contain the number of elements our agent pushed up.
-        expect(redis.llen(key)).to eql event_count
-
-        # Now check all events for order and correctness.
-        event_count.times do |value|
-          id, element = redis.blpop(key, 0)
-          event = LogStash::Event.new(LogStash::Json.load(element))
-          expect(event.get("sequence")).to eql value
-          expect(event.get("message")).to eql message
-        end
-
+        case data_type
+        when "list"
+          expect(redis.llen(key)).to eql event_count
+          # Now check all events for order and correctness.
+          event_count.times do |value|
+            id, element = redis.blpop(key, 0)
+            event = LogStash::Event.new(LogStash::Json.load(element))
+            expect(event.get("sequence")).to eql value
+            expect(event.get("message")).to eql message
+          end
         # The list should now be empty
-        expect(redis.llen(key)).to eql 0
+          expect(redis.llen(key)).to eql 0
+
+        when "set"
+          expect(redis.scard(key)).to eql event_count
+          event_count.times { |value| expect(redis.sismember(key,value)).to be_truthy }
+        end
       end
     end
 
-    context "when batch_mode is false" do
-      include_examples "writing to redis list"
+    set_config = { "set_value" => "%{sequence}" }
+
+    context "when batch_mode is false list" do
+      context "when data_type is list" do
+        let(:data_type) { "list" }
+        include_examples "writing to redis"
+      end
+
+      context "when data_type is set" do
+        let(:data_type) { "set" }
+        include_examples "writing to redis", set_config
+      end
     end
 
-    context "when batch_mode is true" do
+    context "when batch_mode is true " do
       batch_events = Flores::Random.integer(1..1000)
       batch_settings = {
         "batch" => true,
         "batch_events" => batch_events
       }
 
-      include_examples "writing to redis list", batch_settings do
+      context "when data_type is list" do
+        let(:data_type) { "list" }
 
-        # A canary to make sure we're actually enabling batch mode
-        # in this shared example.
-        it "should have batch mode enabled" do
-          expect(redis_config).to include("batch")
-          expect(redis_config["batch"]).to be_truthy
+        include_examples "writing to redis", batch_settings do
+
+          # A canary to make sure we're actually enabling batch mode
+          # in this shared example.
+          it "should have batch mode enabled" do
+            expect(redis_config).to include("batch")
+            expect(redis_config["batch"]).to be_truthy
+          end
+        end
+      end
+
+      context "when data_type is set" do
+        let(:data_type) { "set" }
+
+        include_examples "writing to redis", batch_settings.merge(set_config) do
+          # A canary to make sure we're actually enabling batch mode
+          # in this shared example.
+          it "should have batch mode enabled" do
+            expect(redis_config).to include("batch")
+            expect(redis_config["batch"]).to be_truthy
+          end
         end
       end
     end
